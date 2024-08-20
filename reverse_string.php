9d44c917b9cc14441a2681dea71f49dd6cbba8b9
GisoreBrian <freelancerkazungudev@gmail.com>
2024-08-20 12:39:30 +0300

    PHP serves us with many built-in methods which can be used to manipulate strings.The strrev() function is a built-in function available in PHP and is used to reverse strings.

---
+++ reverse_string.php
@@ -0,0 +1,13 @@
+
+
+<?php
+// PHP program to reverse a string using strrev() 
+
+function Reverse($str){
+    return strrev($str);
+}
+
+// Driver Code 
+$str = "123456789";
+echo Reverse($str)
+?> 
